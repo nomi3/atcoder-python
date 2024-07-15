6b847e7f8f9626cb75f5a3826aa4fb4734528597
nomi3 <thisisnomi3@gmail.com>
2024-07-15 10:27:41 +0900

    尺取法に修正

--- abc360/d/main.py
+++ abc360/d/main.py
@@ -2,20 +2,23 @@ def count_crossings(N, T, S, X):
     ants = [(X[i], S[i]) for i in range(N)]
     ants.sort()  # 蟻の位置でソート
 
-    right_ants = []  # 正の方向に進む蟻のリスト
+    right_ants = []  # 正の方向に進む蟻の位置を保持するリスト
     crossing_count = 0
+    j = 0  # 右向きの蟻の範囲を管理するポインタ
 
-    for position, direction in ants:
+    for i in range(N):
+        position, direction = ants[i]
         if direction == '1':
-            right_ants.insert(0, position)
+            right_ants.append(position)
         else:
             # 左向きの蟻が見つかったら、右向きの蟻とのすれ違いをカウント
-            while right_ants and right_ants[-1] + 2 * T < position:
-                right_ants.pop()  # すれ違わない蟻をリストから削除
-            crossing_count += len(right_ants)
+            while j < len(right_ants) and right_ants[j] + 2 * T < position:
+                j += 1  # 範囲外の右向きの蟻をスキップ
+            crossing_count += len(right_ants) - j
 
     return crossing_count
 
+# 入力の受け取り
 N, T = map(int, input().split())
 S = input()
 X = list(map(int, input().split()))
